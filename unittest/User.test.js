fb53106ecdd515301e8965981b39bae02fec561c
Emil Løkken <emillokken@Emils-MacBook-Pro.local>
2020-12-11 01:03:38 +0100

    forsøg på unit test

---
+++ unittest/User.test.js
@@ -0,0 +1,13 @@
+/*const { User } = require('../model/User');
+
+test('Skulle gerne udskrive en udfyldt user model', () => {
+    const text = User('Emil', 'nekkeol@gmail.com', '21', 'mand', 'kvinde', 'el251099');
+    expect(text).toBe('name', 'Emil', 'email', 'nekkeol@gmail.com', 'age', '21', 'gender', 'mand', 'preferredgender', 'kvinde', 'password', 'el251099')
+})
+
+module.exports = {
+    testEnvironment: 'node'
+  };
+
+  //('name: Emil', 'email: nekkeol@gmail.com', 'age: 21', 'gender: mand', 'preferredgender: kvinde', 'password: el251099')
+  */

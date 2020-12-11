e886aec647bb64cb1850afc6c0d8c8eb2f50a438
Emil Løkken <emillokken@Emils-MacBook-Pro.local>
2020-12-11 04:02:42 +0100

    'rengøring' af fil

--- unittest/User.test.js
+++ unittest/User.test.js
@@ -1,13 +1,12 @@
-/*const { User } = require('../model/User');
+const { User } = require('../model/User');
 
 test('Skulle gerne udskrive en udfyldt user model', () => {
     const text = User('Emil', 'nekkeol@gmail.com', '21', 'mand', 'kvinde', 'el251099');
-    expect(text).toBe('name', 'Emil', 'email', 'nekkeol@gmail.com', 'age', '21', 'gender', 'mand', 'preferredgender', 'kvinde', 'password', 'el251099')
+    expect(text).toBe('name', 'Emil', 'email', 'nekkeol@gmail.com', 'age', '21', 'gender', 
+    'mand', 'preferredgender', 'kvinde', 'password', 'el251099')
 })
 
 module.exports = {
     testEnvironment: 'node'
   };
 
-  //('name: Emil', 'email: nekkeol@gmail.com', 'age: 21', 'gender: mand', 'preferredgender: kvinde', 'password: el251099')
-  */
